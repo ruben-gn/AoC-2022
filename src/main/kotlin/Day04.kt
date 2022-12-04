1da6b1cb1aebb28183840bcc9b777be29310c5d2
Ruben Groot Nibbelink <ruben.grootnibbelink@gmail.com>
2022-12-04 21:26:35 +0100

    Day 04

---
+++ src/main/kotlin/Day04.kt
@@ -0,0 +1,38 @@
+private val example = """
+    2-4,6-8
+    2-3,4-5
+    5-7,7-9
+    2-8,3-7
+    6-6,4-6
+    2-6,4-8
+""".trimIndent()
+
+fun main() = Day04().run()
+//fun main() = Day04().run(example)
+
+class Day04 : Day(4) {
+    override fun part1(input: String): Any =
+        input.split("\n")
+            .map(::toRanges)
+            .count { (one, other) -> one.contains(other) || other.contains(one) }
+
+    override fun part2(input: String): Any =
+        input.split("\n")
+            .map(::toRanges)
+            .count { (one, other) -> one.overlaps(other) }
+
+    private infix fun IntRange.overlaps(other: IntRange): Boolean =
+        other.any { this.contains(it) }
+
+    private fun IntRange.contains(other: IntRange): Boolean =
+        other.all { this.contains(it) }
+
+    private fun toRanges(pairOfRanges: String): List<IntRange> =
+        pairOfRanges.split(",")
+            .map(::toRange)
+            .sortedBy { it.first }
+
+    private fun toRange(range: String): IntRange =
+        range.split("-")[0].toInt()..range.split("-")[1].toInt()
+
+}
